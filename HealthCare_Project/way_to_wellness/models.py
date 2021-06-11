55838d7512691d6370ae2477b108ca339c7a8c09
Prakhar19999 <prakhar.srivastava19999@gmail.com>
2021-06-11 18:45:05 +0530

    Home Page Testimonials Model

--- HealthCare_Project/way_to_wellness/models.py
+++ HealthCare_Project/way_to_wellness/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+class Testimonials(models.Model):
+    name=models.TextField(max_length=30)
+    profession=models.TextField(max_length=500)
+    statement=models.TextField()
+    image=models.ImageField(upload_to='landing_page_testimonials/')
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name_plural='Testimonials'
--- HealthCare_Project/way_to_wellness/views.py
+++ HealthCare_Project/way_to_wellness/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
-
+from .models import *
 
 def home(requests):
-    return render(requests,'way_to_wellness/home_page.html')
+    testimonials=Testimonials.objects.all()
+    context={'testimonials':testimonials}
+    return render(requests,'way_to_wellness/home_page.html',context)
