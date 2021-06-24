2d2afe92b1000cb9cc6f10211f39952c29130be3
Prakhar19999 <prakhar.srivastava19999@gmail.com>
2021-06-25 00:24:59 +0530

    Started Calculators

--- HealthCare_Project/calculators/models.py
+++ HealthCare_Project/calculators/models.py
@@ -1,5 +1,17 @@
 from django.db import models
 
+class Calculator(models.Model):
+    title=models.CharField(max_length=50)
+    paragraph=models.TextField()
+    image=models.ImageField(upload_to='calculators')
+    url_name=models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.title
+    
+    class Meta:
+        verbose_name_plural="Calculators"
+
 class BMI(models.Model):
     weight=models.FloatField()
     height=models.FloatField()
--- HealthCare_Project/calculators/views.py
+++ HealthCare_Project/calculators/views.py
@@ -3,8 +3,10 @@ from .models import *
 from .forms import *
 
 def calculators(requests):
-    bmiform=BMIform()
-    context={'bmiform':bmiform}
+    calculators=Calculator.objects.all()
+    count=0
+    count=Calculator.objects.all().count()
+    context={'calculator':calculators,'count':count}
     return render(requests,'calculators/calculator_base.html',context)
 
 def BMI(requests):
@@ -13,64 +15,65 @@ def BMI(requests):
     if requests.method=='POST':
         bmiform=BMIform(requests.POST)
         if(bmiform.is_valid()):
+            bmiform.save()
             numerator=float(requests.POST.get('weight'))
             denominator=float(requests.POST.get('height'))*float(requests.POST.get('height'))
             bmi=numerator/denominator
     return render(requests,'calculators/BMI.html')
 
 def Water(requests):
-    form=WaterForm()
+    water_form=WaterForm()
     ans=0
     if requests.method =='POST':
-        form=WaterForm(requests.POST)
-        if form.is_valid():
+        water_form=WaterForm(requests.POST)
+        if water_form.is_valid():
+            water_form.save()
             weight=float(requests.POST.get('weight'))
             time=float(requests.POST.get('exercise_time'))
             F1=weight*0.044
             F2=(time/30)*0.355
             ans=F1+F2
-    context={'water_form':form}
-    return render(requests,'calculators/water.html',context)
+    return render(requests,'calculators/water.html')
 
 def WHR(requests):
-    form=WHRform()
+    whr_form=WHRform()
     whr=0
     if requests.method=='POST':
-        form=WHRform(requests.POST)
-        if form.is_valid():
+        whr_form=WHRform(requests.POST)
+        if whr_form.is_valid():
+            whr_form.save()
             num=float(requests.POST.get('waist'))
             den=float(requests.POST.get('hip'))
             whr=num/den
     return render(requests,'calculators/WHR.html')
 
 def BMR(requests):
-    form=BMRform()
+    bmr_form=BMRform()
     bmr_men=0
     bmr_women=0
     if requests.method=='POST':
-        form=BMRform(requests.POST)
-        if form.is_valid():
+        bmr_form=BMRform(requests.POST)
+        if bmr_form.is_valid():
+            bmr_form.save()
             weight=float(requests.POST.get('weight'))
             height=float(requests.POST.get('height'))*2.5
             age=float(requests.POST.get('age'))
             gender=requests.POST.get('gender')
-            print(gender)
             if gender=="Male":
                 bmr_men=655+(9.6*weight)+(1.8*height)-(4.7*age)
             else:
                 bmr_women=66+(13.7*weight)+(5*height)-(6.8*age)
-    print(bmr_men)
-    print(bmr_women)
     return render(requests,'calculators/BMR.html')
 
 def CalMacroNutri(requests):
-    form=NutriForm()
+    nutri_form=NutriForm()
     bmr_men=0
     bmr_women=0
     bmr=0
     if requests.method=='POST':
-        form=NutriForm(requests.POST)
-        if form.is_valid():
+        nutri_form=NutriForm(requests.POST)
+        if nutri_form.is_valid():
+            nutri_form.save()
             weight=float(requests.POST.get('weight'))
             height=float(requests.POST.get('height'))*2.5
             age=float(requests.POST.get('age'))
@@ -95,13 +98,14 @@ def CalMacroNutri(requests):
     return render(requests,'calculators/nutri.html')
 
 def BF(requests):
-    form=BFform()
+    bf_form=BFform()
     male=0
     female=0
     bf=0
     if requests.method=='POST':
-        form=BFform(requests.POST)
-        if form.is_valid():
+        bf_form=BFform(requests.POST)
+        if bf_form.is_valid():
+            bf_form.save()
             gender=requests.POST.get('gender')
             weight=float(requests.POST.get('weight'))
             if gender=="Male":
