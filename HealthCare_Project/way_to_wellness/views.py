2b855378a8811f2265d703be29ed69799d99bc34
Prakhar19999 <prakhar.srivastava19999@gmail.com>
2021-06-26 20:44:24 +0530

    Sending email done

--- HealthCare_Project/way_to_wellness/views.py
+++ HealthCare_Project/way_to_wellness/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
 from .models import *
 from .forms import *
+from django.core.mail import send_mail
+from django.conf import settings
+from django.core.mail import EmailMultiAlternatives
+from django.template.loader import render_to_string
+from django.utils.html import strip_tags
 
 def home(requests):
    form=AppointmentForm()
@@ -8,6 +13,21 @@ def home(requests):
       form=AppointmentForm(requests.POST)
       if form.is_valid():
          form.save()
+         send_mail=requests.POST.get('email')
+         html_content=render_to_string("way_to_wellness/email.html",{"title":'Test Email','content':'Hello'})
+         text_content=strip_tags(html_content)
+         email=EmailMultiAlternatives(
+            #subject
+            'Testing',
+            #content
+            'text_content',
+            #from email
+            settings.EMAIL_HOST_USER,
+            #to email
+            [send_mail,settings.EMAIL_HOST_USER],
+         )
+         email.attach_alternative(html_content,"text/html")
+         email.send()
    form=AppointmentForm()
    testimonials=Testimonials.objects.all()
    carousel=Carousel.objects.all().first()
