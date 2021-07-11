43030c062616894af7e320c3c9087f7107e3a0e6
Prakhar19999 <prakhar.srivastava19999@gmail.com>
2021-07-11 15:10:04 +0530

    SMTP details added

--- HealthCare_Project/HealthCare_Project/settings.py
+++ HealthCare_Project/HealthCare_Project/settings.py
@@ -140,5 +140,5 @@ EMAIL_HOST="smtp.gmail.com"
 EMAIL_PORT=587
 EMAIL_USE_TLS=True
 EMAIL_BACKEND="django.core.mail.backends.smtp.EmailBackend"
-EMAIL_HOST_USER="prakhar.srivastav@orderhealth.in"
-#EMAIL_HOST_PASSWORD="your Smtp Password"
+EMAIL_HOST_USER="amitpathrybiz@gmail.com"
+EMAIL_HOST_PASSWORD="qvpjgafgvadkvzqp"
